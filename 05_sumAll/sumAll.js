3afeb7920133080c7e96c9211d880b969d10ef37
KayaSake <mikehferguson@gmail.com>
2023-02-06 16:34:14 -0300

    Make first 2 tests pass

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,17 @@
-const sumAll = function() {
-
-};
+const sumAll = function(x, y) {
+ if(x<y){ let sum = 0;
+  for (let i = x; i < y + 1; i++) {
+    sum += i;
+  }
+  return sum;
+}
+else if(x>y){ let sum =0;
+  for (let i = y; i < x+1; i++){
+    sum += i;
+  }
+  return sum;
+}
+}
 
 // Do not edit below this line
 module.exports = sumAll;
