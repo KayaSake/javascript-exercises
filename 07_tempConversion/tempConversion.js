9f4d29422f71cf13599fd087507fa16712ac78d7
KayaSake <mikehferguson@gmail.com>
2023-02-08 10:34:45 -0300

    Pass first 4 tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -7,8 +7,13 @@ cel = (cel/10);
 return cel;
 };
 
-const ctof = function() {
-
+const ctof = function(cel) {
+  let fer = 0;
+  fer = ((cel*(9/5))+32);
+  fer = fer*10;
+  fer = Math.round(fer);
+  fer = (fer/10);
+  return fer;
 };
 
 // Do not edit below this line
